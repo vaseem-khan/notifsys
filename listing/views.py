fac53897c7cefaae67d56163159e449a9e57e93e
vaseem-khan <vakhan93@gmail.com>
2014-07-30 17:34:42 +0530

    views done

--- listing/views.py
+++ listing/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 # Create your views here.
 from django.http import HttpResponse
-
+from listing.models import Product
 def index(request):
-    return HttpResponse("views setup done")
+	context=RequestContext(request)
+	product_list=Product.objects.order_by('-time')[:5]
+	context_dict={'products':product_list}
+	return render_to_response('listing/index.html',context_dict,context)
