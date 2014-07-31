e1a1fbbf4936858bd22ecd74c4128631d5a94a7a
vaseem-khan <vakhan93@gmail.com>
2014-07-31 11:02:06 +0530

    Product view created

--- listing/urls.py
+++ listing/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 from listing import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+        url(r'^$', views.index, name='index'),
+        url(r'^product/(?P<product_name_url>\w+)/$', views.products_page, name='products_page'),)
--- listing/views.py
+++ listing/views.py
@@ -3,8 +3,24 @@ from django.template import RequestContext
 # Create your views here.
 from django.http import HttpResponse
 from listing.models import Product
+
 def index(request):
 	context=RequestContext(request)
-	product_list=Product.objects.order_by('-time')[:5]
+	product_list=Product.objects.order_by('-time')[:10]
 	context_dict={'products':product_list}
+	for product in product_list:
+		product.url = product.title.replace(' ', '_')
 	return render_to_response('listing/index.html',context_dict,context)
+
+def products_page(request,product_name_url):
+
+	context = RequestContext(request)
+	product_name=product_name_url.replace('_',' ')
+	context_dict={'product_url':product_name}
+	try:
+		product=Product.objects.get(title=product_name)
+		
+		context_dict['product']=product
+	except Product.DoesNotExist:
+		pass
+	return render_to_response('listing/product.html',context_dict,context)
